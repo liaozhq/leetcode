9ef4cbb833df5e8d475694af7f47c8a6ee122ef9
Gogs <gogs@fake.local>
2019-11-14 12:38:31 +0800

    c

--- src/code1to99/code5.java
+++ src/code1to99/code5.java
@@ -1,7 +1,67 @@
 package code1to99;
 
+import java.util.Arrays;
+
 public class code5 {
-	public String longestPalindrome(String s) {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		System.out.println(longestPalindrome2(
+				"cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj"));
+	    System.out.println(System.currentTimeMillis()-start);
+	}
+
+	public static String longestPalindrome(String s) {
 		char[] chars = s.toCharArray();
+
+		StringBuilder conf = new StringBuilder();
+
+		int max = 0;
+		for (int i = 0; i < chars.length; i++) {
+
+			StringBuilder list = new StringBuilder();
+
+			for (char c : Arrays.copyOfRange(chars, i, chars.length)) {
+
+				list.append(String.valueOf(c));
+
+				StringBuilder builder = new StringBuilder();
+				builder.append(list.toString());
+				builder = builder.reverse();
+				if (list.toString().equals(builder.toString())) {
+					int size = list.toString().toCharArray().length;
+					if (size >= max) {
+						max = size;
+						conf = new StringBuilder();
+						conf.append(list.toString());
+					}
+				}
+			}
+		}
+		return conf.toString();
+	}
+	
+	public static String longestPalindrome2(String s) {
+	    if (s == null || s.length() < 1) return "";
+	    int start = 0, end = 0;
+	    for (int i = 0; i < s.length(); i++) {
+	        int len1 = expandAroundCenter(s, i, i);
+	        int len2 = expandAroundCenter(s, i, i + 1);
+	        int len = Math.max(len1, len2);
+	        if (len > end - start) {
+	            start = i - (len - 1) / 2;
+	            end = i + len / 2;
+	        }
+	    }
+	    return s.substring(start, end + 1);
+	}
+
+	private static int expandAroundCenter(String s, int left, int right) {
+	    int L = left, R = right;
+	    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+	        L--;
+	        R++;
+	    }
+	    return R - L - 1;
 	}
 }
