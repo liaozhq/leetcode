359eb0a438d95b20b244f2e05ffd29bd1ac247dd
qincloud <2877408638@qq.com>
2019-11-18 13:22:49 +0800

    c

---
+++ src/code1to99/code28.java
@@ -0,0 +1,7 @@
+package code1to99;
+
+public class code28 {
+	public int strStr(String haystack, String needle) {
+		char[] chars = haystack.toCharArray();
+	}
+}
---
+++ src/code1to99/code33.java
@@ -0,0 +1,7 @@
+package code1to99;
+
+public class code33 {
+	public int search(int[] nums, int target) {
+
+	}
+}
